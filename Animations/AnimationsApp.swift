8cca1f044580fce0794e28289cf79ff60bda3235
Maraj Hossain <marajhossaincse@gmail.com>
2023-09-12 02:06:41 +0600

    initial commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Maraj Hossain on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
